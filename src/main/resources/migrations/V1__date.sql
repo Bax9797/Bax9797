4f3010108b6e94a3764e0ea60c15e51d6e3f230f
mkalinow-77 <mateuszkalinowski.java@gmail.com>
2022-02-24 21:17:11 +0100

    Some changes

--- src/main/resources/migrations/V1__date.sql
+++
@@ -1,6 +0,0 @@
- insert into  medical_staff(name,surname,medical_specialization, animal_specialization,rate,nip, hired)
- VALUES('Tomasz','Kot','kardiolog','pies',100.00,'54366732',1);
-  insert into  medical_staff(name,surname,medical_specialization, animal_specialization,rate,nip, hired)
-  VALUES('Michal','Koc','kardiolog','kot',200.00,'54336732',1);
-   insert into  medical_staff(name,surname,medical_specialization, animal_specialization,rate,nip, hired)
-   VALUES('Tomasz','Mroz','chirurg','kon',300.00,'54376732',1);
